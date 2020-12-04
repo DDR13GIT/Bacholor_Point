bc8b0e852adbe2ede7dbd1e2fa647a5705d88d47
DDR13GIT <ddroy13@gmail.com>
2020-12-04 23:04:14 +0600

    new features added(report section, about section)

--- Management/src/management/Management.java
+++ Management/src/management/Management.java
@@ -15,7 +15,8 @@ public class Management {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        signIN sg = new signIN();
+        sg.setVisible(true);
     }
     
 }
